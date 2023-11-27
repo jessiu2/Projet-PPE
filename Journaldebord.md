5dd49f990a4f8edfe326cd93a6c5da17bab08d58
Zina-Charikh <zineb.charikh@gmail.com>
2023-11-27 22:23:01 +0100

    journal de bord

--- Journaldebord.md
+++ Journaldebord.md
@@ -1,4 +1,7 @@
-Margot Arnt : https://github.com/margot-a42/Projet-PPE
-Zineb Charikh
-Jing LIU
+- Margot Arnt  https://github.com/margot-a42/Projet-PPE
+- Zineb Charikh
+- Jing LIU
+
+Nous avons choisi le mot "voisin" car il s'agit d'un mot simple utilsé assez couramment. De plus, ce mot a de multiples sens dans la langue française, il désigne quelqu'un qui habite dans le même immeuble/quartier, mais il fait également référence à tout ce qui est à une distance relativement petite.
+
 # Séance 1
---
+++ programmes/premier.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+URLS="../URLs/"
+lineno=1
+
+for file in "$URLS"*
+do
+    while read -r URL
+        do
+            lang=$(basename "$file" .txt)
+            #curl
+            reponse=$(curl -s -I -w "%{http_code}" -o "../aspirations/${lang}-${lineno}.html" "$URL")
+
+            #récupérer dump textuel
+            dumptext=$(lynx -dump -nolist "../dumps-text/${lang}-${lineno}.txt" "$URL")
+
+            #encodage
+            encoding=$(curl -s -I -L -w "%{content_type}" -o /dev/null "$URL" | egrep -E -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
+
+            lineno=$(expr $lineno + 1)
+        done < "$file"
+done
+
+
