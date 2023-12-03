0079327a5eb8109103aa627834ecd185ff5d5b40
Zina-Charikh <zineb.charikh@gmail.com>
2023-12-03 20:51:10 +0100

    ajout contexte

--- programmes/tableau-ar.sh
+++ programmes/tableau-ar.sh
@@ -26,7 +26,7 @@ if [ "$lang" = "arabe" ]; then
         <h1><b>Tableau</b></h1>
         <table border="1">
             <tr>
-                <th>Numero ligne</th><th>URL</th><th>Aspiration</th><th>Dump textuel</th><th>Code HTTP</th><th>Encodage</th><th>Nombre d'occurences</th>
+                <th>Numero ligne</th><th>URL</th><th>Aspiration</th><th>Dump textuel</th><th>Code HTTP</th><th>Encodage</th><th>Nombre d'occurences</th><th>Contexte</th>
             </tr>" > "$tab"
     lineno=1
 
@@ -43,9 +43,13 @@ if [ "$lang" = "arabe" ]; then
 
         occurence=$(egrep -o "$MOT" <<< "$dumptext" | wc -l)
 
+        # Extraire les contextes d'apparition du mot
+        contextes=$(grep -i -C 2 "$MOT" <<< "$dumptext" > "../contextes/contexte_${lang}-${lineno}.txt")
+        cont="../contextes/contexte_${lang}-${lineno}.txt"
+
         echo "<tr>
-                <td>$lineno</td><td><a href="$URL">$URL</a></td><td><a href=\"$asp\">Aspiration</a></td><td><a href=\"$dump\">Dump</a></td><td>$reponse</td><td>$encoding</td><td>$occurence</td>
-            </tr>" >> "$tab"
+
+                <td>$lineno</td><td><a href="$URL">$URL</a></td><td><a href=\"$asp\">Aspiration</a></td><td><a href=\"$dump\">Dump</a></td><td>$reponse</td><td>$encoding</td><td>$occurence</td><td><a href=\"$cont\">Voir contexte</a></td></tr>" >> "$tab"
 
         lineno=$(expr $lineno + 1)
     done < "$URLS"
